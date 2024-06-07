e94d3247575890b1cc9547446ec7cba6b399baa0
Keegan Micu <LemonTag73@gmail.com>
2024-06-07 17:11:16 -0400

    everything is working but the login is not working, not sure if the stuff is saving

--- server/server.js
+++ server/server.js
@@ -6,12 +6,12 @@ const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 const PORT = process.env.PORT || 3001;
-const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+const app = express();
 
 const startApolloServer = async () => {
   await server.start();
